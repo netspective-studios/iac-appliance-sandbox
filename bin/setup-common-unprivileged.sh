d29e0f87e513ceeb7ce445bb4e08cfe292494d5d
Shahid N. Shah <shahid@shah.org>
2021-02-12 21:40:42 -0500

    feat: integrate privileged user

--- bin/setup-common-unprivileged.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-source `dirname $0`/setup-common-configure.sh
-
-# TODO: if a Ansible playbook doesn't need super user, put it in this script
